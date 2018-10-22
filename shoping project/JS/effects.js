3d49a01783dc18bebaf0f2bd90294d091ec5dc02
himanijain176 <32105202+himanijain176@users.noreply.github.com>
2018-10-22 19:01:25 +0530

    Update effects.js

--- shoping project/JS/effects.js
+++ shoping project/JS/effects.js
@@ -1,4 +1,5 @@
-filterSelection("all")
+
+/*filterSelection("all")
 function filterSelection(c) {
   var x, i;
   x = document.getElementsByClassName("column");
@@ -41,7 +42,7 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
-
+*/
 function logform() {
 	var username = document.forms["loginForm"]["usrname"].value;
 	var password = document.forms["loginForm"]["psw"].value;
@@ -54,4 +55,56 @@ function logform() {
     alert("Invalid username or password");
     document.getElementById("errordisplay").innerHTML="Invalid username or password."
 	}
+}
+var fetchData = function(){
+	$.get("./Productdata.json",function(data,status){
+
+		for(var d of data){
+      console.log(d)
+			var item = document.createElement('div');
+			item.setAttribute("class","col-lg-4 col-md-4 col-sm-6 col-xs-12");
+			item.setAttribute("id","item"+d.id);
+			document.getElementById('display-row').appendChild(item);
+			
+			var card = document.createElement('div');
+			card.setAttribute('class','card');
+			card.setAttribute('id','card'+d.id);
+			document.getElementById("item"+d.id).appendChild(card);
+			
+			var image = document.createElement('img');
+			image.setAttribute('class','card-img-top');
+			image.setAttribute('src',d.image[0]);
+			document.getElementById('card'+d.id).appendChild(image);
+
+			var cBody = document.createElement('div');
+			cBody.setAttribute('class','card-body');
+			cBody.setAttribute('id','body'+d.id);
+			document.getElementById('card'+d.id).appendChild(cBody);
+
+			var nameField = document.createElement('h4');
+			nameField.setAttribute('class','title');
+			nameField.setAttribute('id','card-title'+d.id);
+			document.getElementById('card'+d.id).appendChild(nameField);
+			document.getElementById('card-title'+d.id).innerHTML = d.name;
+
+			var price = document.createElement('p');
+			price.setAttribute('class','price');
+			price.setAttribute('id','card-price'+d.id);
+			document.getElementById('card'+d.id).appendChild(price);
+			document.getElementById('card-price'+d.id).innerHTML ="Rs. "+ d.price;
+
+			var btn = document.createElement('button');
+			btn.setAttribute('class','btn btn-success');
+			btn.setAttribute('id','btnWish'+d.id);
+			document.getElementById('card'+d.id).appendChild(btn);
+			document.getElementById('btnWish'+d.id).innerHTML = "Add to Wishlist";
+
+			var Addbtn = document.createElement('button');
+			Addbtn.setAttribute('class','btn btn-primary');
+			Addbtn.setAttribute('id','btnCart'+d.id);
+			document.getElementById('card'+d.id).appendChild(Addbtn);
+			document.getElementById('btnCart'+d.id).innerHTML = "Add to Cart";
+
+		}
+	});
 }
