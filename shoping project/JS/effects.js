e2174f3a2a87fe6812c5d6f057349fbc4632f382
himanijain176 <32105202+himanijain176@users.noreply.github.com>
2018-10-24 12:11:14 +0530

    Update effects.js

--- shoping project/JS/effects.js
+++ shoping project/JS/effects.js
@@ -46,161 +46,143 @@ for (var i = 0; i < btns.length; i++) {
 function logform() {
 	var username = document.forms["loginForm"]["usrname"].value;
 	var password = document.forms["loginForm"]["psw"].value;
-	if (username == "Himani" && password=="admin") {
+	if (username == "Himani" && password == "admin") {
 		console.log("Hello");
-		window.location="adminpage.html";
+		window.location = "adminpage.html";
 	}
-	else
-	{
-    alert("Invalid username or password");
-    document.getElementById("errordisplay").innerHTML="Invalid username or password."
+	else {
+		alert("Invalid username or password");
+		document.getElementById("errordisplay").innerHTML = "Invalid username or password."
 	}
 }
 
-var fetchData = function(){
+var fetchData = function () {
 	callmenumethod();
-	$.getJSON("./product.json",function(result){
-		$.each(result.productcat, function(i,category){
-				var subMenuData='';
-				$.each(category.sub_category, function(i,sub_categories){
-						subMenuData += "<li class='naveffect' style='list-style-type: none'><a  onClick= 'displayProduct($(this).text())'>"+sub_categories.typename+"</a></li>";
-	
-				});
-						var mainMenuData ="<li class='naveff'><a>"+category.category+
-						"</a><ul class='sub-menu'>"+subMenuData+"</ul></li>";
-				$(mainMenuData).appendTo("#secondnav");
-				console.log(mainMenuData);
+	$.getJSON("./product.json", function (result) {
+		$.each(result.productcat, function (i, category) {
+			var subMenuData = '';
+			$.each(category.sub_category, function (i, sub_categories) {
+				subMenuData += "<li class='naveffect' style='list-style-type: none'><a  onClick= 'displayProduct($(this).text())'>" + sub_categories.typename + "</a></li>";
+
+			});
+			var mainMenuData = "<li class='naveff'><a>" + category.category +
+				"</a><ul class='sub-menu'>" + subMenuData + "</ul></li>";
+			$(mainMenuData).appendTo("#secondnav");
+			console.log(mainMenuData);
 		});
 		$(".sub-menu").hide();
-					$("#secondnav li").not($('#secondnav li sub-menu li a')).click(function () {
-							$('ul.sub-menu').not( $(this).children() ).slideUp();
-							$(this).children("ul.sub-menu").slideToggle();
-				
-					});
-			});
-		};
-	function callmenumethod(){
-	$.get("./Productdata.json",function(data,status){
+		$("#secondnav li").not($('#secondnav li sub-menu li a')).click(function () {
+			$('ul.sub-menu').not($(this).children()).slideUp();
+			$(this).children("ul.sub-menu").slideToggle();
 
-		for(var d of data){
-      console.log(d)
+		});
+	});
+};
+function callmenumethod() {
+	$.get("./Productdata.json", function (data, status) {
+
+		for (var d of data) {
+			console.log(d)
 			var item = document.createElement('div');
-			item.setAttribute("class","col-lg-4 col-md-4 col-sm-6 col-xs-12");
-			item.setAttribute("id","item"+d.id);
+			item.setAttribute("class", "col-lg-4 col-md-4 col-sm-6 col-xs-12");
+			item.setAttribute("id", "item" + d.id);
 			document.getElementById('display-row').appendChild(item);
-			
+
 			var card = document.createElement('div');
-			card.setAttribute('class','card');
-			card.setAttribute('id','card'+d.id);
-			document.getElementById("item"+d.id).appendChild(card);
-			
+			card.setAttribute('class', 'card');
+			card.setAttribute('id', 'card' + d.id);
+			document.getElementById("item" + d.id).appendChild(card);
+
 			var image = document.createElement('img');
-			image.setAttribute('class','card-img-top');
-			image.setAttribute('src',d.image[0]);
-			document.getElementById('card'+d.id).appendChild(image);
+			image.setAttribute('class', 'card-img-top');
+			image.setAttribute('src', d.image[0]);
+			document.getElementById('card' + d.id).appendChild(image);
 
 			var cBody = document.createElement('div');
-			cBody.setAttribute('class','card-body');
-			cBody.setAttribute('id','body'+d.id);
-			document.getElementById('card'+d.id).appendChild(cBody);
+			cBody.setAttribute('class', 'card-body');
+			cBody.setAttribute('id', 'body' + d.id);
+			document.getElementById('card' + d.id).appendChild(cBody);
 
 			var nameField = document.createElement('h4');
-			nameField.setAttribute('class','title');
-			nameField.setAttribute('id','card-title'+d.id);
-			document.getElementById('card'+d.id).appendChild(nameField);
-			document.getElementById('card-title'+d.id).innerHTML = d.name;
+			nameField.setAttribute('class', 'title');
+			nameField.setAttribute('id', 'card-title' + d.id);
+			document.getElementById('card' + d.id).appendChild(nameField);
+			document.getElementById('card-title' + d.id).innerHTML = d.name;
 
 			var price = document.createElement('p');
-			price.setAttribute('class','price');
-			price.setAttribute('id','card-price'+d.id);
-			document.getElementById('card'+d.id).appendChild(price);
-			document.getElementById('card-price'+d.id).innerHTML ="Rs. "+ d.price;
+			price.setAttribute('class', 'price');
+			price.setAttribute('id', 'card-price' + d.id);
+			document.getElementById('card' + d.id).appendChild(price);
+			document.getElementById('card-price' + d.id).innerHTML = "Rs. " + d.price;
 
 			var btn = document.createElement('button');
-			btn.setAttribute('class','btn btn-info');
-			btn.setAttribute('id','btnWish'+d.id);
-			document.getElementById('card'+d.id).appendChild(btn);
-			document.getElementById('btnWish'+d.id).innerHTML = "Add to Wishlist";
+			btn.setAttribute('class', 'btn btn-info');
+			btn.setAttribute('id', 'btnWish' + d.id);
+			document.getElementById('card' + d.id).appendChild(btn);
+			document.getElementById('btnWish' + d.id).innerHTML = "Add to Wishlist";
 
 			var Addbtn = document.createElement('button');
-			Addbtn.setAttribute('class','btn btn-danger');
-			Addbtn.setAttribute('id','btnCart'+d.id);
-			document.getElementById('card'+d.id).appendChild(Addbtn);
-			document.getElementById('btnCart'+d.id).innerHTML = "Add to Cart";
+			Addbtn.setAttribute('class', 'btn btn-danger');
+			Addbtn.setAttribute('id', 'btnCart' + d.id);
+			document.getElementById('card' + d.id).appendChild(Addbtn);
+			document.getElementById('btnCart' + d.id).innerHTML = "Add to Cart";
 
 		}
 	});
 }
-function displayProduct(subcategorydata){
-	console.log("hi");
-	var subcat=subcategorydata;
-	var newsubcat=[];
-	$.get("./Productdata.json",function(data,status){
-		for(var da of data)
-		{
-			if(subcat.localeCompare(da.sub_category)==0)
-			{
-				console.log("true");
-				newsubcat.push(subcat);
-				for(var e of newsubcat)
-					{
-						console.log("hello again");
-						$('#display-row').empty();
-					var item = document.createElement('div');
-					item.setAttribute("class","col-lg-4 col-md-4 col-sm-6 col-xs-12");
-					item.setAttribute("id","item"+da.id);
-					document.getElementById('display-row').appendChild(item);
-					
-					var card = document.createElement('div');
-					card.setAttribute('class','card');
-					card.setAttribute('id','card'+da.id);
-					document.getElementById("item"+da.id).appendChild(card);
-					
-					var image = document.createElement('img');
-					image.setAttribute('class','card-img-top');
-					image.setAttribute('src',da.image[0]);
-					document.getElementById('card'+da.id).appendChild(image);
-		
-					var cBody = document.createElement('div');
-					cBody.setAttribute('class','card-body');
-					cBody.setAttribute('id','body'+da.id);
-					document.getElementById('card'+da.id).appendChild(cBody);
-		
-					var nameField = document.createElement('h4');
-					nameField.setAttribute('class','title');
-					nameField.setAttribute('id','card-title'+da.id);
-					document.getElementById('card'+da.id).appendChild(nameField);
-					document.getElementById('card-title'+da.id).innerHTML = da.name;
-		
-					var price = document.createElement('p');
-					price.setAttribute('class','price');
-					price.setAttribute('id','card-price'+da.id);
-					document.getElementById('card'+da.id).appendChild(price);
-					document.getElementById('card-price'+da.id).innerHTML ="Rs. "+ da.price;
-		
-					var btn = document.createElement('button');
-					btn.setAttribute('class','btn btn-info');
-					btn.setAttribute('id','btnWish'+da.id);
-					document.getElementById('card'+da.id).appendChild(btn);
-					document.getElementById('btnWish'+da.id).innerHTML = "Add to Wishlist";
-		
-					var Addbtn = document.createElement('button');
-					Addbtn.setAttribute('class','btn btn-danger');
-					Addbtn.setAttribute('id','btnCart'+da.id);
-					document.getElementById('card'+da.id).appendChild(Addbtn);
-					document.getElementById('btnCart'+da.id).innerHTML = "Add to Cart";
-							}
-					
-				
-			
-				console.log(subcat);
-			}
-			else{
-				console.log("false");
+function displayProduct(subcategorydata) {
+	var newsubcat = [];
+	$.get("./Productdata.json", function (data, status) {
+		$('#display-row').empty();
+		for (var da of data) {
+			if (subcategorydata == da.sub_category) {
+				var item = document.createElement('div');
+				item.setAttribute("class", "col-lg-4 col-md-4 col-sm-6 col-xs-12");
+				item.setAttribute("id", "item" + da.id);
+				document.getElementById('display-row').appendChild(item);
+
+				var card = document.createElement('div');
+				card.setAttribute('class', 'card');
+				card.setAttribute('id', 'card' + da.id);
+				document.getElementById("item" + da.id).appendChild(card);
+
+				var image = document.createElement('img');
+				image.setAttribute('class', 'card-img-top');
+				image.setAttribute('src', da.image[0]);
+				document.getElementById('card' + da.id).appendChild(image);
+
+				var cBody = document.createElement('div');
+				cBody.setAttribute('class', 'card-body');
+				cBody.setAttribute('id', 'body' + da.id);
+				document.getElementById('card' + da.id).appendChild(cBody);
+
+				var nameField = document.createElement('h4');
+				nameField.setAttribute('class', 'title');
+				nameField.setAttribute('id', 'card-title' + da.id);
+				document.getElementById('card' + da.id).appendChild(nameField);
+				document.getElementById('card-title' + da.id).innerHTML = da.name;
+
+				var price = document.createElement('p');
+				price.setAttribute('class', 'price');
+				price.setAttribute('id', 'card-price' + da.id);
+				document.getElementById('card' + da.id).appendChild(price);
+				document.getElementById('card-price' + da.id).innerHTML = "Rs. " + da.price;
+
+				var btn = document.createElement('button');
+				btn.setAttribute('class', 'btn btn-info');
+				btn.setAttribute('id', 'btnWish' + da.id);
+				document.getElementById('card' + da.id).appendChild(btn);
+				document.getElementById('btnWish' + da.id).innerHTML = "Add to Wishlist";
+
+				var Addbtn = document.createElement('button');
+				Addbtn.setAttribute('class', 'btn btn-danger');
+				Addbtn.setAttribute('id', 'btnCart' + da.id);
+				document.getElementById('card' + da.id).appendChild(Addbtn);
+				document.getElementById('btnCart' + da.id).innerHTML = "Add to Cart";
 			}
 		}
 
-	
+
 	});
 }
 
