8ed6c707a4bb4b61dc3aa7eba57cbd6fd3ba505c
himanijain176 <32105202+himanijain176@users.noreply.github.com>
2018-10-26 13:05:00 +0530

    Update cart.js

--- shoping project/JS/cart.js
+++ shoping project/JS/cart.js
@@ -1,4 +1,5 @@
 var fetchcartData=function(){
+    
 var proddetail=localStorage.getItem('cart').split(',');
 $.getJSON("./Productdata.json", function (result) {
     proddetail.forEach(function(item){
@@ -6,19 +7,35 @@ $.getJSON("./Productdata.json", function (result) {
         {
             if(item==d.id)
             {
-                console.log(d.id);
-                document.getElementById("hello").src=d.image[0];
+                console.log(item);
+                /*document.getElementById("hello").src=d.image[0];
                 document.getElementById("itemname").innerHTML=""+d.name;
                 document.getElementById("pricedetail").innerHTML=+d.price;
-                document.getElementById("brandinfo").innerHTML=""+d.brand;
-             }
-    
-        }
+                document.getElementById("brandinfo").innerHTML=""+d.brand;*/
+                var cartitem = document.createElement('div');
+			    cartitem.setAttribute('class', 'col-lg-4 col-md-4 col-sm-6 col-xs-12');
+			    cartitem.setAttribute('id', 'cartitem' + d.id);
+			    document.getElementById('display-cart').appendChild(cartitem);
 
-});
-});
+			
+
+			var cartimage = document.createElement('img');
+			cartimage.setAttribute('class', 'cartimg');
+            cartimage.setAttribute('src', d.image[0]);
+         
+			document.getElementById('cartitem' + d.id).appendChild(image);
+
+             var cartdata=document.createElement('table');
+			cartdata.setAttribute('class', 'carttable');
+            cartdata.setAttribute('id', 'carttable' + d.id);
+            document.getElementById('cartitem' + d.id).appendChild(carttable);
+            document.getElementById('carttable' + d.id).innerHTML="<tr><td>Name:</td><td>"+d.name+"</td></tr><tr><td>Price</td><td>"+d.price+"</td></tr><tr><td>Brand:</td><td>"+d.brand+"</td></tr>";
+ 
+            
+			
+
 }
-function deletedata(item)
-{
-       localStorage.removeItem('cart',item);
+        }
+    });
+});
 }
