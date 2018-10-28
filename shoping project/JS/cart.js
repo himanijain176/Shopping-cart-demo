08a329f1b9ac7884df8cce36fa9c05de66e72dbf
himanijain176 <32105202+himanijain176@users.noreply.github.com>
2018-10-28 16:24:42 +0530

    Update cart.js

--- shoping project/JS/cart.js
+++ shoping project/JS/cart.js
@@ -1,41 +1,53 @@
+/*Code to Add to cart and delete from Cart*/
+
 var fetchcartData=function(){
-    
+    var totalprice=0;
 var proddetail=localStorage.getItem('cart').split(',');
 $.getJSON("./Productdata.json", function (result) {
-    proddetail.forEach(function(item){
+    
         for(var d of result)
         {
+            for(var item of proddetail){
             if(item==d.id)
             {
                 console.log(item);
+                var cartItem = document.createElement('div');
+                    cartItem.setAttribute("class", "cartItem");
+                    cartItem.setAttribute("id", "cartItem" + d.id);
+                    document.getElementById('display-cart').appendChild(cartItem);
+                    
+                    var img = document.createElement('img');
+                    img.setAttribute('id', 'cartimg' + d.id);
+                    img.setAttribute('src', d.image[0]);
+                    document.getElementById('cartItem' + d.id).appendChild(img);
+
+                    var itemDesc = document.createElement('div');
+                    itemDesc.setAttribute("class", "itemDesc");
+                    itemDesc.setAttribute("id", "items" + d.id);
+                    document.getElementById('cartItem' + d.id).appendChild(itemDesc);
+
+                    var cartab = document.createElement('table');
+                    cartab.setAttribute("id", "cartab" + d.id);
+                    document.getElementById('items' + d.id).appendChild(cartab);
+
+                    document.getElementById('cartab' + d.id).innerHTML = '<tr><th><h3 id="name' + d.id + '">Name :</h3></th></tr ><tr><th><h3 id="price' + d.id + '">Price :</h3></th></tr><tr><th><h3 id="brand' + d.id + '">Brand :</h3></th></tr><tr><td><input type="button" class="btn btn-danger remove" value="Remove from cart" onClick="removeProductCart('+d.id+')" id = '+d.id+'/></td><td><input type="button" class="btn btn-success success" value="Buy Now" /></td></tr>'
+                    document.getElementById('name'+d.id).innerHTML = "Name : "+d.name;
+                    document.getElementById('price'+d.id).innerHTML = "Price : "+d.price;
+                    document.getElementById('brand'+d.id).innerHTML = "Brand : "+d.brand;
+                    totalprice+=eval(d.price);
                 /*document.getElementById("hello").src=d.image[0];
                 document.getElementById("itemname").innerHTML=""+d.name;
                 document.getElementById("pricedetail").innerHTML=+d.price;
                 document.getElementById("brandinfo").innerHTML=""+d.brand;*/
-                var cartitem = document.createElement('div');
-			    cartitem.setAttribute('class', 'col-lg-4 col-md-4 col-sm-6 col-xs-12');
-			    cartitem.setAttribute('id', 'cartitem' + d.id);
-			    document.getElementById('display-cart').appendChild(cartitem);
-
-			
-
-			var cartimage = document.createElement('img');
-			cartimage.setAttribute('class', 'cartimg');
-            cartimage.setAttribute('src', d.image[0]);
-         
-			document.getElementById('cartitem' + d.id).appendChild(image);
-
-             var cartdata=document.createElement('table');
-			cartdata.setAttribute('class', 'carttable');
-            cartdata.setAttribute('id', 'carttable' + d.id);
-            document.getElementById('cartitem' + d.id).appendChild(carttable);
-            document.getElementById('carttable' + d.id).innerHTML="<tr><td>Name:</td><td>"+d.name+"</td></tr><tr><td>Price</td><td>"+d.price+"</td></tr><tr><td>Brand:</td><td>"+d.brand+"</td></tr>";
- 
-            
+             
 			
 
 }
+
         }
-    });
+    }
+var pricedis=document.createElement('div');
+
+document.getElementById('totalPrice').innerHTML="Total Price : "+totalprice;
 });
 }
