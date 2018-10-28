1ab63c8944655d064669d17eecb10132fd5de762
himanijain176 <32105202+himanijain176@users.noreply.github.com>
2018-10-28 17:31:00 +0530

    Update cart.js

--- shoping project/JS/cart.js
+++ shoping project/JS/cart.js
@@ -2,7 +2,7 @@
 
 var fetchcartData=function(){
     var totalprice=0;
-var proddetail=localStorage.getItem('cart').split(',');
+    var proddetail=localStorage.getItem('cart').split(',');
 $.getJSON("./Productdata.json", function (result) {
     
         for(var d of result)
@@ -30,7 +30,7 @@ $.getJSON("./Productdata.json", function (result) {
                     cartab.setAttribute("id", "cartab" + d.id);
                     document.getElementById('items' + d.id).appendChild(cartab);
 
-                    document.getElementById('cartab' + d.id).innerHTML = '<tr><th><h3 id="name' + d.id + '">Name :</h3></th></tr ><tr><th><h3 id="price' + d.id + '">Price :</h3></th></tr><tr><th><h3 id="brand' + d.id + '">Brand :</h3></th></tr><tr><td><input type="button" class="btn btn-danger remove" value="Remove from cart" onClick="removeProductCart('+d.id+')" id = '+d.id+'/></td><td><input type="button" class="btn btn-success success" value="Buy Now" /></td></tr>'
+                    document.getElementById('cartab' + d.id).innerHTML = '<tr><th><h3 id="name' + d.id + '">Name :</h3></th></tr ><tr><th><h3 id="price' + d.id + '">Price :</h3></th></tr><tr><th><h3 id="brand' + d.id + '">Brand :</h3></th></tr><tr><td><input type="button" class="btn btn-danger remove" value="delete" onClick="DeleteProduct('+d.id+')" id = '+d.id+'/></td><td><input type="button" class="btn btn-success success" value="Buy Now" /></td></tr>'
                     document.getElementById('name'+d.id).innerHTML = "Name : "+d.name;
                     document.getElementById('price'+d.id).innerHTML = "Price : "+d.price;
                     document.getElementById('brand'+d.id).innerHTML = "Brand : "+d.brand;
@@ -51,3 +51,25 @@ var pricedis=document.createElement('div');
 document.getElementById('totalPrice').innerHTML="Total Price : "+totalprice;
 });
 }
+function DeleteProduct(item)
+{
+    console.log(item);
+    //var pid=item.slice(-2);
+    //console.log(pid);
+    var itemdata="0"+item;
+    console.log(itemdata);
+    var newprod=[];
+   
+        var proddetail=localStorage.getItem('cart').split(',');
+       
+        console.log(proddetail);
+             newprod=proddetail.remove(itemdata);
+            console.log(newprod);
+            
+
+    
+
+
+    //fetchcartData();
+
+    }
